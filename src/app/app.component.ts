400b2e2fe7e505222df5aabf16caf052c9db014a
Daniel Abati <danielabatibabatunde1@gmail.com>
2022-09-12 09:56:08 +0100

    Push-notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, ChangeDetectorRef , Component } from '@angular/core';
+import { OneSignal } from 'onesignal-ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,21 @@ import { AfterViewInit, ChangeDetectorRef , Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit{
-  constructor(private cd: ChangeDetectorRef) {
-
+  constructor(private cd: ChangeDetectorRef, private onesignal:OneSignal) {
+    this.onesignal.init({
+      appId: "65503543-7f3f-4440-b1c0-62dced4533ea",
+      serviceWorkerParam : {
+        scope: 'src/OneSignalSDKWorker.js'
+      },
+     serviceWorkerPath: 'src/OneSignalSDKWorker.js'
+    }).then((value)=>{
+      console.log({value})
+    }).catch((err)=>{
+     console.log(err)
+    });
+    this.onesignal.on('subscriptionChange', function(isSubscribed) {
+      console.log("The user's subscription state is now:", isSubscribed);
+    });
   }
 
   ngAfterViewInit(): void {
@@ -17,3 +31,5 @@ export class AppComponent implements AfterViewInit{
 
   spinner:any;
 }
+
+
